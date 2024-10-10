7d4a1257f6f6dff8855b3ee42121120295a1ff46
mytac <my_tac@126.com>
2024-10-10 11:50:37 +0800

    updated to chapter 9

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,8 +14,7 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,6 +29,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -47,6 +47,7 @@ export async function fetchLatestInvoices() {
     throw new Error('Failed to fetch the latest invoices.');
   }
 }
+
 
 export async function fetchCardData() {
   try {
